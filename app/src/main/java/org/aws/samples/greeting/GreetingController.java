d1eb904ecdd772c93b7e6701c66f252843545c2f
Ragini Movva <raginimovva@Raginis-MacBook-Air.local>
2022-07-11 16:05:59 -0700

    Adding stack files

---
+++ app/src/main/java/org/aws/samples/greeting/GreetingController.java
@@ -0,0 +1,17 @@
+package org.aws.samples.greeting;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author MaheshR
+ */
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello from AWS ProServe version2";
+    }
+   
+}
